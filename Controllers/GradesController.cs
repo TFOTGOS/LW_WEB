55581fecaf5b30ff3e3574d93b492de16da22140
TFOTGOS <tfotgos@gmail.com>
2023-12-20 20:56:21 +0200

    Initial commit

---
+++ Controllers/GradesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LW_WEB.Models;
+
+namespace LW_WEB.Controllers
+{
+    public class GradesController : Controller
+    {
+        private readonly LwWebContext _context;
+
+        public GradesController(LwWebContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Grades
+        public async Task<IActionResult> Index()
+        {
+            var lwWebContext = _context.Grades.Include(g => g.Enrollment);
+            return View(await lwWebContext.ToListAsync());
+        }
+
+        // GET: Grades/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades
+                .Include(g => g.Enrollment)
+                .FirstOrDefaultAsync(m => m.GradeId == id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+
+            return View(grade);
+        }
+
+        // GET: Grades/Create
+        public IActionResult Create()
+        {
+            ViewData["EnrollmentId"] = new SelectList(_context.Enrollments, "EnrollmentId", "EnrollmentId");
+            return View();
+        }
+
+        // POST: Grades/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GradeId,EnrollmentId,GradeValue,ExamDate")] Grade grade)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(grade);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EnrollmentId"] = new SelectList(_context.Enrollments, "EnrollmentId", "EnrollmentId", grade.EnrollmentId);
+            return View(grade);
+        }
+
+        // GET: Grades/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades.FindAsync(id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+            ViewData["EnrollmentId"] = new SelectList(_context.Enrollments, "EnrollmentId", "EnrollmentId", grade.EnrollmentId);
+            return View(grade);
+        }
+
+        // POST: Grades/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("GradeId,EnrollmentId,GradeValue,ExamDate")] Grade grade)
+        {
+            if (id != grade.GradeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(grade);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GradeExists(grade.GradeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["EnrollmentId"] = new SelectList(_context.Enrollments, "EnrollmentId", "EnrollmentId", grade.EnrollmentId);
+            return View(grade);
+        }
+
+        // GET: Grades/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var grade = await _context.Grades
+                .Include(g => g.Enrollment)
+                .FirstOrDefaultAsync(m => m.GradeId == id);
+            if (grade == null)
+            {
+                return NotFound();
+            }
+
+            return View(grade);
+        }
+
+        // POST: Grades/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var grade = await _context.Grades.FindAsync(id);
+            if (grade != null)
+            {
+                _context.Grades.Remove(grade);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GradeExists(int id)
+        {
+            return _context.Grades.Any(e => e.GradeId == id);
+        }
+    }
+}
